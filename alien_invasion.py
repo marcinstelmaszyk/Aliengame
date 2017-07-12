0764fbf3feaf1cac15873beee14a2fe3f1423d36
Marcin <stelmaszyk.m@gmail.com>
2017-07-12 19:03:01 +0200

    Window background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,10 +6,16 @@ def run_game():
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Inwazja obcych")
 
+    # Zdefiniowanie koloru tła
+    bg_color = (230, 230, 230)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
+
         pygame.display.flip()
 
 run_game()
