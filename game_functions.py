00ca8db95cc0ecdcfb3819251324204494ac8017
Marcin <stelmaszyk.m@gmail.com>
2017-07-13 20:03:07 +0200

    Game functions moved to separate file.

---
+++ game_functions.py
@@ -0,0 +1,16 @@
+import sys
+
+import pygame
+
+def check_events():
+    '''Reakcja na zdarzenia generowane przez klawiaturę i mysz.'''
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    '''Uaktualnienie obrazów na ekranie i przejście do nowego ekranu.'''
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+       
+    pygame.display.flip()
