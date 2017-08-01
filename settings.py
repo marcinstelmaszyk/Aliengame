e62a7574167877bf6695dcd3ab5858cd5cae4ef5
Marcin <stelmaszyk.m@gmail.com>
2017-08-01 18:38:04 +0200

    Add firing bullets

--- settings.py
+++ settings.py
@@ -9,3 +9,10 @@ class Settings():
 
         # Ustawienie dotyczące statku
         self.ship_speed_factor = 1.5
+
+        # Ustawienia dotyczące pocisku
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
